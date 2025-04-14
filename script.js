3b7f8cd541cfb62e5ff06f1985ed3f6b9b9f698c
Anityasundar <anityasundar53@gmail.com>
2025-04-14 22:55:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+gitHubURL = "https://api.github.com/users/Anityasundar"
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log("Portfolio site loaded successfully!");
+
+  const data = fetch(gitHubURL).then(res => res.json()).catch(err => console.error("Error: ", err)).finally();
+  
+  const githubSection = document.getElementById("github-data")
+  
+  if(data){
+
+    githubSection.innerHTML = `<h2><a href="${data.html_url}">My GitHub Profile</a></h2>`
+  }
 });
